66796c721663fb64051ba3b3b90a48e6776e14d8
Trofin Vlad <79583000+godvlader@users.noreply.github.com>
2022-11-29 15:06:41 +0100

    update webhook

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ intents.presences = True
 
 intents = discord.Intents.default()
 intents.message_content=True
-#webhook update
+#webhook update 2
 """
 Uncomment this if you don't want to use prefix (normal) commands.
 It is recommended to use slash commands and therefore not use prefix commands.
